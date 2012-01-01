548d4e62f3b914ad3806b7bd651801bbf23365b1
AoJ <hegenbart@allin1.cz>
2012-01-01 23:28:30 +0100

    +now js core

--- server.js
+++ server.js
@@ -10,25 +10,21 @@ var _ = require('underscore');
 var config = JSON.parse(fs.readFileSync(__dirname+ '/config.json', 'utf-8'));
 var seed = JSON.parse(fs.readFileSync(__dirname+ '/db/schema.json', 'utf-8'));
 
-
-function serveStartpage(req, res) {
-  html = fs.readFileSync(__dirname+ '/templates/app.html', 'utf-8');
-  res.send(html.replace('{{{{seed}}}}', JSON.stringify(seed))
-               .replace('{{{{session}}}}', JSON.stringify(req.session)));  
-}
-
-// Express.js Configuration
-// -----------
-
 app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(app.router);
   app.use(express.static(__dirname+"/public", { maxAge: 41 }));
-  app.use(express.logger({ format: ':method :url' }));
-
 });
 
-
 app.listen(8276);
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(app);
+      
+everyone.now.distribute = function(message){
+  // this.now exposes caller's scope
+  everyone.now.receive(this.now.name, message);
+};
+
