cf42c7cd3cca82c087780b7d09301bf02d13d07d
AoJ <hegenbart@allin1.cz>
2012-01-06 22:19:10 +0100

    simplest and now

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.get('x:name', function(req, res){
 
 });
 
-app.get(':id/__log', function(req, res){
-	var name = req.params.name;
+app.get('everyone/__log', function(req, res){
+	var name = req.params.name || 'everyone';
 	var group = nowjs.getGroup(name);
 
 	//add new group if not exists
