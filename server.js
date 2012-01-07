c4cce6406d252403759910661b102994ab1eaec7
AoJ <hegenbart@allin1.cz>
2012-01-07 12:12:46 +0100

    refactor

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.configure(function() {
 var	logger = require('./logger').initialize(app)
 ,	homepage = fs.readFileSync(__dirname+ '/public/index.html', 'utf-8');
 
-console.log(logger);
+
 
 /* ----- ROUTES -----*/
 
@@ -30,6 +30,7 @@ app.get('/:name/__log', function(req, res, next){
 
 
 app.get('/:name', function(req, res){
+	console.log(logger);
 	logger.log(req.params);
 	var name = req.params.name || 'unknown';
 
